90d16ba907ea7eb8b7c1eac2e06f8b2e4e26d33d
Richard Huang <rzhuangsg115@gmail.com>
2021-03-25 15:51:30 -0500

    Add checkGuess and askGuess

--- guessing_game.js
+++ guessing_game.js
@@ -1,7 +1,31 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
 let secretnumber = 5;
 
+
 let checkGuess = function (num) {
-
-
-
+    if (secretnumber < num){
+        console.log("too high");
+        return false;
+    }
+    else if(secretnumber > num){
+        console.log("too low");
+        return false;
+    }
+    else{
+        console.log("correct");
+        return true;
+    }
 }
+
+let askGuess = function(){
+    rl.question('Enter a guess: ', checkGuess(answer) => {
+
+        rl.close();
+      });
+}
+askGuess();
